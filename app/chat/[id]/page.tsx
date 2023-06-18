b82f6e3d46c1e6fbbad97fd61386b98ff261b229
Solmaz <solmaz.mousavie@gmail.com>
2023-06-18 08:04:03 +0200

    ChatInput component

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,9 +1,11 @@
 import Chat from "@/components/Chat";
+import ChatInput from "@/components/ChatInput";
 
 export default function ChatPage() {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
       <Chat />
+      <ChatInput />
     </div>
   );
 }
